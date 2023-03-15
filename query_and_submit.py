4a2e229a42b8eb7f044269c49d60eabe00a6c87f
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2023-03-15 16:11:30 +0100

    USEID-915: Enable export per record

--- query_and_submit.py
+++ query_and_submit.py
@@ -21,19 +21,49 @@ interval_end = one_hour_ago.strftime("%Y-%m-%dT%H:59:59Z")
 metrics = {}
 
 for metric_name, query in config["queries"].items():
-  sql = query["countSql"]
-  condition_keyword = "and" if re.search("where", sql, re.IGNORECASE) else "where"
+  count_sql = query.get("countSql")
+  content_sql = query.get("contentSql")
   timestamp_col = query["timestampColumn"]
-  metrics[metric_name] = json.loads(subprocess.check_output(["psql", os.environ["DBURI"], "-Atc", f"{sql} {condition_keyword} \"{timestamp_col}\" between '{interval_start}' and '{interval_end}'"]))
+  
+  if content_sql:
+    condition_keyword = "and" if re.search("where", content_sql, re.IGNORECASE) else "where"
+    db_result = subprocess.check_output(["psql", os.environ["DBURI"], "-Atc", f"{content_sql} {condition_keyword} \"{timestamp_col}\" between '{interval_start}' and '{interval_end}'"]).decode().split("\n")
+    db_result = filter(lambda x: x != "", db_result)
 
-requests.post(
-  url=config["endpoint"],
-  json={
-    "startInterval": interval_start,
-    "endInterval": interval_end,
-    **metrics,
-  },
-  headers={
-    "xc-token": metrics_token,
-  }
-)
+    content_metric = {}
+    for result in db_result:
+      category, content, created_at = result.split("|")
+
+      content_metric["category"] = category
+      content_metric["content"] = content
+      content_metric["date"] = created_at
+
+      json_output = {
+        "createdAt": interval_start,
+        "endInterval": interval_end,
+        **content_metric
+      }
+
+      requests.post(
+        url=config["endpoint"],
+        json=json_output,
+        headers={
+          "xc-token": metrics_token,
+        }
+      )
+
+  else:
+    condition_keyword = "and" if re.search("where", count_sql, re.IGNORECASE) else "where"
+    metrics[metric_name] = json.loads(subprocess.check_output(["psql", os.environ["DBURI"], "-Atc", f"{count_sql} {condition_keyword} \"{timestamp_col}\" between '{interval_start}' and '{interval_end}'"]))
+
+    requests.post(
+      url=config["endpoint"],
+      json={
+        "startInterval": interval_start,
+        "endInterval": interval_end,
+        **metrics,
+      },
+      headers={
+        "xc-token": metrics_token,
+      }
+    )
