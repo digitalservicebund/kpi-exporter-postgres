aa70200654ac6a926d15e4b28dfb8d2d075c8e1c
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2023-06-06 15:09:38 +0200

    Revert "USEID-915: Enable export per record (#1)"
    This reverts commit 77b2e481606369b5311298bb6f77d7da71d60dec.

--- query_and_submit.py
+++ query_and_submit.py
@@ -18,52 +18,22 @@ one_hour_ago = dt.datetime.now() - dt.timedelta(hours=1)
 interval_start = one_hour_ago.strftime("%Y-%m-%dT%H:00:00Z")
 interval_end = one_hour_ago.strftime("%Y-%m-%dT%H:59:59Z")
 
-count_metrics = {}
-metrics = []
+metrics = {}
 
 for metric_name, query in config["queries"].items():
-  count_sql = query.get("countSql")
-  content_sql = query.get("contentSql")
+  sql = query["countSql"]
+  condition_keyword = "and" if re.search("where", sql, re.IGNORECASE) else "where"
   timestamp_col = query["timestampColumn"]
-  
-  if content_sql:
-    condition_keyword = "and" if re.search("where", content_sql, re.IGNORECASE) else "where"
-    db_result = subprocess.check_output(["psql", os.environ["DBURI"], "-Atc", f"{content_sql} {condition_keyword} \"{timestamp_col}\" between '{interval_start}' and '{interval_end}'"]).decode().split("\n")
-    db_result = filter(lambda single_result: single_result != "", db_result)
+  metrics[metric_name] = json.loads(subprocess.check_output(["psql", os.environ["DBURI"], "-Atc", f"{sql} {condition_keyword} \"{timestamp_col}\" between '{interval_start}' and '{interval_end}'"]))
 
-    content_metric = {}
-
-    for result in db_result:
-      category, content, created_at = result.split("|")
-
-      content_metric["category"] = category
-      content_metric["content"] = content
-      content_metric["date"] = created_at
-
-      metrics.append({
-        **content_metric
-      })
-
-  else:
-    condition_keyword = "and" if re.search("where", count_sql, re.IGNORECASE) else "where"
-    count_metrics[metric_name] = json.loads(subprocess.check_output(["psql", os.environ["DBURI"], "-Atc", f"{count_sql} {condition_keyword} \"{timestamp_col}\" between '{interval_start}' and '{interval_end}'"]))
-
-if count_metrics:
-  metrics.append({
+requests.post(
+  url=config["endpoint"],
+  json={
     "startInterval": interval_start,
     "endInterval": interval_end,
-    **count_metrics,
-  })
-
-response = requests.post(
-  url=config["endpoint"],
-  json=metrics,
+    **metrics,
+  },
   headers={
     "xc-token": metrics_token,
   }
 )
-
-if response.ok:
-  print("export done")
-else:
-  print("export failed")
